1515c30730adbcf3e45b2597dde253726104c8d7
MaximilianProll <maximilian.proll@hotmail.de>
2018-11-10 15:13:07 +0200

    preliminary CNN model - proof of concept

--- CNN.py
+++ CNN.py
@@ -1,7 +1,10 @@
 import pandas as pd
-import numpy as np
 
 from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+from keras import regularizers, optimizers
 
 
 def split_dataframe(df, train_p, val_p,  random_state=200):
@@ -28,9 +31,13 @@ def split_dataframe(df, train_p, val_p,  random_state=200):
 
 if __name__ == '__main__':
     path_to_csv = 'nests.csv'
+    path_to_img = 'data/Max_20Flights2017/frames/'
     train_p, val_p = 0.8, 0.1
     index_col = 'files'
     label_col = 'nest'
+    img_size = (336, 256)
+    color_mode = 'grayscale'
+    class_mode = 'categorical'
     batch_size = 32
 
     df = pd.read_csv(path_to_csv)
@@ -42,51 +49,89 @@ if __name__ == '__main__':
 
     train_generator = datagen.flow_from_dataframe(
         dataframe=df_train,
-        directory='data/Max_20Flights2017/frames/',
+        directory=path_to_img,
         x_col=index_col,
         y_col=label_col,
         has_ext=True,
-        target_size=(336, 256),
-        color_mode='grayscale',
-        class_mode='other',
+        target_size=img_size,
+        color_mode=color_mode,
+        class_mode=class_mode,
         batch_size=batch_size,
         # shuffle=True
     )
 
     val_generator = datagen.flow_from_dataframe(
         dataframe=df_val,
-        directory='data/Max_20Flights2017/frames/',
+        directory=path_to_img,
         x_col=index_col,
         y_col=label_col,
         has_ext=True,
-        target_size=(336, 256),
-        color_mode='grayscale',
-        class_mode='other',
+        target_size=img_size,
+        color_mode=color_mode,
+        class_mode=class_mode,
         batch_size=batch_size,
         # shuffle=True
     )
 
     test_generator = datagen.flow_from_dataframe(
         dataframe=df_test,
-        directory='data/Max_20Flights2017/frames/',
+        directory=path_to_img,
         x_col=index_col,
         y_col=label_col,
         has_ext=True,
-        target_size=(336, 256),
-        color_mode='grayscale',
-        class_mode='other',
+        target_size=img_size,
+        color_mode=color_mode,
+        class_mode=class_mode,
         batch_size=batch_size,
         # shuffle=True
     )
 
-    i = 0
-    for x, y in train_generator:
-        print('x ', x.shape, 'y ', y.shape)
-        if i > 10:
-            break
-        i += 1
+    # build model
 
+    model = Sequential(name='CNN')
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     input_shape=img_size + (1,), activation='relu'))
+    model.add(BatchNormalization())
 
-    print()
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(2, activation='softmax'))
+
+    model.summary()
+
+    model.compile(optimizers.rmsprop(lr=0.0001, decay=1e-6), loss="categorical_crossentropy", metrics=["accuracy"])
+
+    # fitting the model
+    STEP_SIZE_TRAIN = train_generator.n // train_generator.batch_size
+    STEP_SIZE_VALID = val_generator.n // val_generator.batch_size
+
+    model.fit_generator(generator=train_generator,
+                        steps_per_epoch=STEP_SIZE_TRAIN,
+                        validation_data=val_generator,
+                        validation_steps=STEP_SIZE_VALID,
+                        epochs=10,
+                        verbose=1
+                        )
+
+    # evaluate the model
+    score = model.evaluate_generator(generator=valid_generator)
+
+    # print loss and accuracy
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
 
